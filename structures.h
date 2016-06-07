04c8e8d9c3007417c4efd37c6b515639b79a392c
elise kocik <elise.kocik@gmail.com>
2016-06-07 20:59:47 +0200

    structure train

--- structures.h
+++ structures.h
@@ -1,17 +1,41 @@
+/**********************************************
+* Librairie pour la gestion de trains	      *
+* Projet LO41 - Printemps 2016				  *
+* Auteurs : 								  *
+* Florian Bishop et Elise Kocik 			  *
+**********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
 #ifndef STRUCTURES_H
 #define	STRUCTURES_H
+
+/**********************************************
+* Définition du type Train         			  *
+**********************************************/
 
 typedef enum {
     EO,
     OE
 } Direction;
 
+typedef enum {
+    TGV,
+    GL,
+    M
+} Type;
+
 typedef struct {
-    char *nom;
+    Type type;
     Direction direction;
-    int priorite;
+    int id;
+    char* destination;
 } Train;
 
-
+/**********************************************
+* Fonctions sur les Trains         			  *
+**********************************************/
 
 #endif	/* STRUCTURES_H */
