8cbf2362307b649a604abfd7cc8ba552bd45b63d
elise kocik <elise.kocik@gmail.com>
2016-06-07 21:09:26 +0200

    Train.h

---
+++ train.h
@@ -0,0 +1,50 @@
+/**********************************************
+* Librairie pour la gestion de trains	      *
+* Projet LO41 - Printemps 2016				  *
+* Auteurs : 								  *
+* Florian Bishop et Elise Kocik 			  *
+**********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#ifndef TRAIN_H
+#define	TRAIN_H
+
+/**********************************************
+* Définition du type Train         			  *
+**********************************************/
+
+typedef enum {
+    EO,
+    OE
+} Direction;
+
+typedef enum {
+    TGV,
+    GL,
+    M
+} Type;
+
+typedef struct {
+    Type type;
+    Direction direction;
+    int id;
+    char* destination;
+} Train;
+
+/**********************************************
+* Fonctions sur les Trains         			  *
+**********************************************/
+/** Crée un nouveau train avec l'id et le type spécifié
+* La destination est aléatoire*/
+Train* createTrain(int id, Type type, Direction dir);
+
+/**Crée un train aléatoire**/
+Train* randomTrain(int id);
+
+/** Suppression du train*/
+void deleteTrain(Train *t);
+
+#endif	/* STRUCTURES_H */
